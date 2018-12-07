c668f58a32e6930e30900c6397cad3864bb951b9
Orell Hürzeler <orell.huerzeler@gmail.com>
2018-12-07 08:27:16 +0100

    update

--- saxoton/portfolio/views.py
+++ saxoton/portfolio/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Gigs, FeaturedWorks
+from .models import Gigs, FeaturedWorks, GalleryPicture
 
 def home(request):
-    gigs = Gigs.objects.all()
+    gigs = Gigs.objects.all().order_by('Date')
     featured = FeaturedWorks.objects.all()
     return render(request, 'home.html', {'gigs': gigs, 'featured':featured})
 
 def gallerie(request):
-    return render(request, 'gallerie.html',)
+    items = GalleryPicture.objects.all()
+    return render(request, 'gallerie.html', {'items': items})
