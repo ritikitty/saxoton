f71e92c9f5842e8fc061f16040932dfe6876299f
Orell Hürzeler <orell.huerzeler@gmail.com>
2018-12-12 22:41:24 +0100

    updated

--- saxoton/saxoton/settings.py
+++ saxoton/saxoton/settings.py
@@ -15,10 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# data stuff
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -124,7 +120,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
+if DEBUG:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+    # data stuff
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/home/photosyntheticarts/saxoton/saxoton/mvp_static/'
+    STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+
+    # data stuff
+    MEDIA_ROOT = '/home/photosyntheticarts/saxoton/saxoton/mvp_static/media/'
+    MEDIA_URL = '/media/'
